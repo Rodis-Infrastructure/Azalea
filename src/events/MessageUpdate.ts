2f66cc0af093cff817541cf40d24ea466ff4feb3
nick <59822256+Archasion@users.noreply.github.com>
2024-05-13 15:34:29 +0100

    fix(message-update): Remove import

--- src/events/MessageUpdate.ts
+++ src/events/MessageUpdate.ts
@@ -12,7 +12,6 @@ import {
     prependReferenceLog,
     formatMessageContentForLog,
     Messages,
-    resolvePartialMessage,
     prepareMessageForStorage,
     formatMessageLogEntry
 } from "@utils/messages";
