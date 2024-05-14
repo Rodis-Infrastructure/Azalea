712521dc1d4d7773ac8ab8137b0e10a6b8eaa626
nick <59822256+Archasion@users.noreply.github.com>
2024-05-14 22:28:47 +0100

    fix: Infraction pagination

--- src/components/InfractionSearchNext.ts
+++ src/components/InfractionSearchNext.ts
@@ -23,9 +23,9 @@ export default class InfractionSearchNext extends Component {
  */
 export async function handleInfractionSearchPagination(interaction: ButtonInteraction<"cached">, pageOffset: number): Promise<InteractionReplyData> {
     // ID of the user that initiated the search
-    const searchExecutorId = interaction.message.interaction?.user.id ?? (await interaction.message.fetchReference()).author.id;
+    const searchExecutorId = interaction.message.interaction?.user.id;
 
-    if (interaction.user.id !== searchExecutorId && searchExecutorId !== client.user.id) {
+    if (searchExecutorId && interaction.user.id !== searchExecutorId) {
         return {
             content: "You do not have permission to use pagination as you did not initiate this search",
             ephemeral: true
