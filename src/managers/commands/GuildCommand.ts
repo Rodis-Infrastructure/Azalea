c4e190f6856cddaad4137b4c479f4bcfc1c7dfad
nick <59822256+Archasion@users.noreply.github.com>
2024-06-05 18:29:58 +0100

    refactor(ts-config): Enable  'noImplicitOverride'

--- src/managers/commands/GuildCommand.ts
+++ src/managers/commands/GuildCommand.ts
@@ -9,7 +9,7 @@ export default abstract class GuildCommand<T extends CommandInteraction> extends
      * @param data The data for the command.
      * @protected
      */
-    protected constructor(public readonly config: GuildConfig, public data: ApplicationCommandData) {
+    protected constructor(public readonly config: GuildConfig, public override data: ApplicationCommandData) {
         super(data);
     }
 }
