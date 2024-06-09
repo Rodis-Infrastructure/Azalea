b9bc6edcc13b41e186de5a6dc0a6a9d97094cca2
nick <59822256+Archasion@users.noreply.github.com>
2024-06-09 13:06:38 +0100

    refactor(infraction-search-ctx): Add permission check

--- src/commands/SearchInfractionsCtx.ts
+++ src/commands/SearchInfractionsCtx.ts
@@ -3,6 +3,8 @@ import { InteractionReplyData } from "@utils/types";
 
 import Infraction, { InfractionSearchFilter } from "./Infraction";
 import Command from "@managers/commands/Command";
+import ConfigManager from "@managers/config/ConfigManager";
+import { Permission } from "@managers/config/schema";
 
 export default class SearchInfractionsCtx extends Command<UserContextMenuCommandInteraction> {
     constructor() {
@@ -13,6 +15,20 @@ export default class SearchInfractionsCtx extends Command<UserContextMenuCommand
     }
 
     execute(interaction: UserContextMenuCommandInteraction<"cached">): Promise<InteractionReplyData> {
+        const config = ConfigManager.getGuildConfig(interaction.guildId, true);
+        const member = interaction.targetMember;
+
+        if (
+            member &&
+            config.hasPermission(member, Permission.ViewInfractions) &&
+            !config.hasPermission(interaction.member, Permission.ViewModerationActivity)
+        ) {
+            return Promise.resolve({
+                content: "You do not have permission to view this user's infractions.",
+                ephemeral: true
+            });
+        }
+
         return Infraction.search({
             user: interaction.targetUser,
             guildId: interaction.guildId,
