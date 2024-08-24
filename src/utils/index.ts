4914e953e93974ad0d05f54f07e5336ef00fd59e
nick <59822256+Archasion@users.noreply.github.com>
2024-08-24 15:28:00 +0100

    refactor(getSurfaceName): Use different separator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -286,7 +286,7 @@ export function getSurfaceName(member: GuildMember | User): string {
         return `@${username}`;
     }
 
-    return `@${username} (${displayName})`;
+    return `@${username} | ${displayName}`;
 }
 
 /**
