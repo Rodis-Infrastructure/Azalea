5aa67c2bb876677f75c555dcf148a5a661c6aa53
nick <59822256+Archasion@users.noreply.github.com>
2024-10-29 11:44:35 +0000

    chore(dependencies): Update discord.js to 14.16.3

--- src/managers/commands/Command.ts
+++ src/managers/commands/Command.ts
@@ -1,31 +1,41 @@
-import { ApplicationCommandData, AutocompleteInteraction, CommandInteraction } from "discord.js";
+import {
+	ApplicationCommandData,
+	ApplicationIntegrationType,
+	AutocompleteInteraction,
+	CommandInteraction,
+	InteractionContextType
+} from "discord.js";
+
 import { DEFAULT_COMMAND_PERMISSIONS, DEFAULT_DM_PERMISSION } from "@utils/constants";
 import { InteractionReplyData } from "@utils/types";
 
 // The base class for all commands.
 export default abstract class Command<T extends CommandInteraction> {
 	/**
-     * @param data The data for the command.
-     * @protected
-     */
-	protected constructor(public readonly data: ApplicationCommandData) {}
+	 * @param data The data for the command.
+	 * @protected
+	 */
+	protected constructor(public readonly data: ApplicationCommandData) {
+	}
 
-    /**
-     * Handles the command interaction. Mentions are disabled by default.
-     * @param interaction The interaction to handle.
-     */
-    abstract execute(interaction: T): InteractionReplyData | Promise<InteractionReplyData>;
+	/**
+	 * Handles the command interaction. Mentions are disabled by default.
+	 * @param interaction The interaction to handle.
+	 */
+	abstract execute(interaction: T): InteractionReplyData | Promise<InteractionReplyData>;
 
-    /**
-     * Handles the associated autocomplete interaction.
-     * @param interaction The interaction to handle.
-     */
-    autocomplete?(interaction: AutocompleteInteraction): Promise<void> | void;
+	/**
+	 * Handles the associated autocomplete interaction.
+	 * @param interaction The interaction to handle.
+	 */
+	autocomplete?(interaction: AutocompleteInteraction): Promise<void> | void;
 
-    build(): ApplicationCommandData {
-    	this.data.defaultMemberPermissions ??= DEFAULT_COMMAND_PERMISSIONS;
-    	this.data.dmPermission ??= DEFAULT_DM_PERMISSION;
+	build(): ApplicationCommandData {
+		this.data.defaultMemberPermissions ??= DEFAULT_COMMAND_PERMISSIONS;
+		this.data.dmPermission ??= DEFAULT_DM_PERMISSION;
+		this.data.contexts ??= [InteractionContextType.Guild];
+		this.data.integrationTypes ??= [ApplicationIntegrationType.GuildInstall];
 
-    	return this.data;
-    }
+		return this.data;
+	}
 }
